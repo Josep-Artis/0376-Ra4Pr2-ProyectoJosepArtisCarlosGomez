b584bd784e70e5fc844b02315dfb51d066c9e544
JosepArtis <jartis25@ilb.cat>
2026-03-19 15:59:13 +0100

    segunda funcion + comprobacion de que funciona.

--- script.js
+++ script.js
@@ -18,3 +18,37 @@ console.log("Codigo secreto:", CodigoSecreto);
 
 //Codigo secreto: [3, 7, 1, 9]
 //borrar hasta aqui o dejar como comentario.
+
+
+// FUNCIÓN 2: Algoritmo de validación (dos pasadas)
+function validarIntento(intentoUsuario, codigoSecreto) {
+    const pistas = [];           // aquí guardamos las pistas resultado: 1, Ø, ×
+    const codigoPendiente = [];  // copia del código secreto para "tachar" números ya usados
+    const intentoPendiente = []; // copia del intento para "tachar" números ya usados
+    // PRIMERA PASADA: buscar posicion y numeros correctos 
+    for (let i = 0; i < 4; i++) {
+        if (intentoUsuario[i] === codigoSecreto[i]) {
+            pistas.push('1');
+            codigoPendiente.push(null);   // tachamos el número del código secreto
+            intentoPendiente.push(null);  // tachamos el número del intento
+        } else {
+            codigoPendiente.push(codigoSecreto[i]);   // lo dejamos pendiente
+            intentoPendiente.push(intentoUsuario[i]); // lo dejamos pendiente
+        }
+    }
+    // --- SEGUNDA PASADA: buscar números correctos mal colocados (Ø)
+    for (let i = 0; i < 4; i++) {
+        if (intentoPendiente[i] === null) continue; // ya fue marcado como '1', lo saltamos
+        const pos = codigoPendiente.indexOf(intentoPendiente[i]); // ¿existe este número en el código?
+        if (pos !== -1) {
+            pistas.push('Ø');
+            codigoPendiente[pos] = null; // lo tachamos para no reutilizarlo
+        } else {
+            pistas.push('×');
+        }
+    }
+    return pistas;
+}
+//console.log(validarIntento([1, 2, 3, 4], [1, 2, 3, 4])); console.log de prueba de consola para ver si funciona.
+
+
